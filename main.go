39702f8bf7cab4aed8174c0262ff7a75f640a8f8
Richard Pham <hellorichardpham@gmail.com>
2020-10-24 19:26:33 -0700

    main characters and their VAs are brought in

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -34,8 +35,40 @@ type Anime struct {
 	Popularity   int     `json:"popularity"`
 }
 
+type CharacterSearch struct {
+	Characters []Character `json:"characters"`
+}
+
+type Character struct {
+	MalID       int          `json:"mal_id"`
+	URL         string       `json:"url"`
+	ImageURL    string       `json:"image_url"`
+	Name        string       `json:"name"`
+	Role        string       `json:"role"`
+	VoiceActors []VoiceActor `json:"voice_actors"`
+
+	// VoiceActors []VoiceActor `json:"voice_actors"`
+}
+
+type VoiceActor struct {
+	MalID    int    `json:"mal_id"`
+	URL      string `json:"url"`
+	Name     string `json:"name"`
+	ImageURL string `json:"image_url"`
+	Language string `json:"language"`
+}
+
 func getAnimeSearch(body []byte) (*AnimeSearch, error) {
 	var s = new(AnimeSearch)
+	err := json.Unmarshal(body, &s)
+	if err != nil {
+		fmt.Println("whoops:", err)
+	}
+	return s, err
+}
+
+func getCharacterSearch(body []byte) (*CharacterSearch, error) {
+	var s = new(CharacterSearch)
 	err := json.Unmarshal(body, &s)
 	if err != nil {
 		fmt.Println("whoops:", err)
@@ -50,11 +83,6 @@ func getAnime(body []byte) (*Anime, error) {
 		fmt.Println("whoops:", err)
 	}
 	return s, err
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the homepage!")
-	fmt.Println("Endpoint Hit: homepage")
 }
 
 func returnAnimeSearch(w http.ResponseWriter, r *http.Request) {
@@ -116,12 +144,63 @@ func returnSingleAnime(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(animeSingle)
 }
 
+func returnMainCharacters(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	malID := vars["id"]
+
+	apiCharacterStaffURL := "https://api.jikan.moe/v3/anime/" + malID + "/characters_staff"
+	fmt.Println("apiCharacterStaffURL: " + apiCharacterStaffURL)
+	resp, err := http.Get(apiCharacterStaffURL)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status: ", resp.Status)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	characterSearch, err := getCharacterSearch([]byte(body))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var tempCharacters []Character
+	for _, c := range characterSearch.Characters {
+		if c.Role == "Main" {
+			var tempCharacter = new(Character)
+			tempCharacter.MalID = c.MalID
+			tempCharacter.URL = c.URL
+			tempCharacter.ImageURL = c.ImageURL
+			tempCharacter.Name = c.Name
+			tempCharacter.Role = c.Role
+			tempCharacter.VoiceActors = c.VoiceActors
+			for i := range tempCharacter.VoiceActors {
+				// tempCharacter.VoiceActors[i].ImageURL = "HELLO"
+				// fmt.Println("Before: " + va.ImageURL)
+				tempCharacter.VoiceActors[i].ImageURL = strings.Replace(tempCharacter.VoiceActors[i].ImageURL, "r/42x62/", "", -1)
+				// fmt.Println("After: " + va.ImageURL)
+			}
+			tempCharacters = append(tempCharacters, *tempCharacter)
+		}
+	}
+
+	fmt.Println("tempCharacters ", tempCharacters)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(tempCharacters)
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/search", returnAnimeSearch)
 	myRouter.HandleFunc("/anime/{id}", returnSingleAnime)
+	myRouter.HandleFunc("/anime/{id}/mainCharacters", returnMainCharacters)
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      animes: [{"mal_id":38101,"url":"https://myanimelist.net/anime/38101/5-toubun_no_Hanayome","image_url":"https://cdn.myanimelist.net/images/anime/1819/97947.jpg?s=63766d471cbb06b0bc8d4c81d5fc98fe","title":"5-toubun no Hanayome"},{"mal_id":39783,"url":"https://myanimelist.net/anime/39783/5-toubun_no_Hanayome_∬","image_url":"https://cdn.myanimelist.net/images/anime/1775/109514.jpg?s=7223653b9c57b539ca603e6881f0b9af","title":"5-toubun no Hanayome ∬"},{"mal_id":39650,"url":"https://myanimelist.net/anime/39650/Essential_Free___Easy","image_url":"https://cdn.myanimelist.net/images/anime/1245/100364.jpg?s=e328d369f0f02481d5c62ddae1e2ba21","title":"Essential Free & Easy"},{"mal_id":34239,"url":"https://myanimelist.net/anime/34239/Mutsugo_to_Ouma_no_Monogatari","image_url":"https://cdn.myanimelist.net/images/anime/10/82374.jpg?s=b31e227e890c15534a05819d881426fe","title":"Mutsugo to Ouma no Monogatari"},{"mal_id":37277,"url":"https://myanimelist.net/anime/37277/Beatless_Intermission","image_url":"https://cdn.myanimelist.net/images/anime/1709/90468.jpg?s=ec76696930b074eb49a21cfaa607a2a6","title":"Beatless Intermission"},{"mal_id":5682,"url":"https://myanimelist.net/anime/5682/Phantom__Requiem_for_the_Phantom","image_url":"https://cdn.myanimelist.net/images/anime/8/22470.jpg?s=9c291938aeadd872feac14621d5b11e8","title":"Phantom: Requiem for the Phantom"},{"mal_id":3759,"url":"https://myanimelist.net/anime/3759/School_Days__Valentine_Days","image_url":"https://cdn.myanimelist.net/images/anime/4/21483.jpg?s=fc79afe3cc0e5197f467adcb420b0467","title":"School Days: Valentine Days"},{"mal_id":1143,"url":"https://myanimelist.net/anime/1143/hack__Intermezzo","image_url":"https://cdn.myanimelist.net/images/anime/12/66499.jpg?s=2ba754c2e7a617e4fc6fa5f95d7421fe","title":".hack//Intermezzo"},{"mal_id":34818,"url":"https://myanimelist.net/anime/34818/Sakura_Internet","image_url":"https://cdn.myanimelist.net/images/anime/8/84025.jpg?s=5f19024a82e502fadb589eaea1a56c02","title":"Sakura Internet"},{"mal_id":40272,"url":"https://myanimelist.net/anime/40272/A3_Season_Autumn___Winter","image_url":"https://cdn.myanimelist.net/images/anime/1049/108197.jpg?s=f74763942434d27422453beda5364c24","title":"A3! Season Autumn & Winter"}],
-      selectedAnime: {"mal_id":38101,"url":"https://myanimelist.net/anime/38101/5-toubun_no_Hanayome","image_url":"https://cdn.myanimelist.net/images/anime/1819/97947.jpg","title":"5-toubun no Hanayome","title_english":"The Quintessential Quintuplets","score":7.57,"scored_by":190793,"rank":1370,"popularity":355},
+      animes: [{ "mal_id": 38101, "url": "https://myanimelist.net/anime/38101/5-toubun_no_Hanayome", "image_url": "https://cdn.myanimelist.net/images/anime/1819/97947.jpg?s=63766d471cbb06b0bc8d4c81d5fc98fe", "title": "5-toubun no Hanayome" }, { "mal_id": 39783, "url": "https://myanimelist.net/anime/39783/5-toubun_no_Hanayome_∬", "image_url": "https://cdn.myanimelist.net/images/anime/1775/109514.jpg?s=7223653b9c57b539ca603e6881f0b9af", "title": "5-toubun no Hanayome ∬" }, { "mal_id": 39650, "url": "https://myanimelist.net/anime/39650/Essential_Free___Easy", "image_url": "https://cdn.myanimelist.net/images/anime/1245/100364.jpg?s=e328d369f0f02481d5c62ddae1e2ba21", "title": "Essential Free & Easy" }, { "mal_id": 34239, "url": "https://myanimelist.net/anime/34239/Mutsugo_to_Ouma_no_Monogatari", "image_url": "https://cdn.myanimelist.net/images/anime/10/82374.jpg?s=b31e227e890c15534a05819d881426fe", "title": "Mutsugo to Ouma no Monogatari" }, { "mal_id": 37277, "url": "https://myanimelist.net/anime/37277/Beatless_Intermission", "image_url": "https://cdn.myanimelist.net/images/anime/1709/90468.jpg?s=ec76696930b074eb49a21cfaa607a2a6", "title": "Beatless Intermission" }, { "mal_id": 5682, "url": "https://myanimelist.net/anime/5682/Phantom__Requiem_for_the_Phantom", "image_url": "https://cdn.myanimelist.net/images/anime/8/22470.jpg?s=9c291938aeadd872feac14621d5b11e8", "title": "Phantom: Requiem for the Phantom" }, { "mal_id": 3759, "url": "https://myanimelist.net/anime/3759/School_Days__Valentine_Days", "image_url": "https://cdn.myanimelist.net/images/anime/4/21483.jpg?s=fc79afe3cc0e5197f467adcb420b0467", "title": "School Days: Valentine Days" }, { "mal_id": 1143, "url": "https://myanimelist.net/anime/1143/hack__Intermezzo", "image_url": "https://cdn.myanimelist.net/images/anime/12/66499.jpg?s=2ba754c2e7a617e4fc6fa5f95d7421fe", "title": ".hack//Intermezzo" }, { "mal_id": 34818, "url": "https://myanimelist.net/anime/34818/Sakura_Internet", "image_url": "https://cdn.myanimelist.net/images/anime/8/84025.jpg?s=5f19024a82e502fadb589eaea1a56c02", "title": "Sakura Internet" }, { "mal_id": 40272, "url": "https://myanimelist.net/anime/40272/A3_Season_Autumn___Winter", "image_url": "https://cdn.myanimelist.net/images/anime/1049/108197.jpg?s=f74763942434d27422453beda5364c24", "title": "A3! Season Autumn & Winter" }],
+      selectedAnime: { "mal_id": 38101, "url": "https://myanimelist.net/anime/38101/5-toubun_no_Hanayome", "image_url": "https://cdn.myanimelist.net/images/anime/1819/97947.jpg", "title": "5-toubun no Hanayome", "title_english": "The Quintessential Quintuplets", "score": 7.57, "scored_by": 190793, "rank": 1370, "popularity": 355 },
+      mainCharacters: [],
       error: false
     }
   }
@@ -56,12 +57,33 @@ class Search extends React.Component {
           });
         }
       )
+
+    fetch("http://localhost:10000/anime/" + malId + "/mainCharacters")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log("voice actors: " + JSON.stringify(result))
+          this.setState({
+            mainCharacters: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            error: true
+          });
+        }
+      )
   }
 
   render() {
-    const { animes, selectedAnime, error } = this.state;
+    const { animes, selectedAnime, mainCharacters, error } = this.state;
     console.log('I am rendering with the state animes: ' + this.state.animes);
     console.log('I am rendering with the state selectedAnime: ' + this.state.selectedAnime.title_english);
+    console.log('I am rendering with the state mainCharacters.length: ' + this.state.mainCharacters.length);
+
     if (error) {
       return (
         <div>There was an error.</div>
@@ -69,18 +91,18 @@ class Search extends React.Component {
     } else {
       return (
         <div className="parent">
-            <SearchBar
-              onClick={title => this.handleSearchAction(title)}
-            />
+          <SearchBar
+            onClick={title => this.handleSearchAction(title)}
+          />
           <Flex container margin="10px auto" justifyContent="space-around" flexWrap="wrap">
-              {animes.map(anime => (
-                  <SearchResult malId={anime.mal_id}
-                    title={anime.title}
-                    url={anime.url}
-                    imageUrl={anime.image_url}
-                    onClick={malId => this.handleAnimeSelected(malId)} />
-              ))}
-            </Flex>
+            {animes.map(anime => (
+              <SearchResult malId={anime.mal_id}
+                title={anime.title}
+                url={anime.url}
+                imageUrl={anime.image_url}
+                onClick={malId => this.handleAnimeSelected(malId)} />
+            ))}
+          </Flex>
           <AnimeResult
             malId={selectedAnime.mal_id}
             imageUrl={selectedAnime.image_url}
@@ -91,6 +113,18 @@ class Search extends React.Component {
             rank={selectedAnime.rank}
             popularity={selectedAnime.popularity}
           />
+          <Flex container margin="10px auto" flexWrap="wrap">
+          {mainCharacters.map(character => (
+            <MainCharacter
+              malID={character.mal_id}
+              name={character.name}
+              url={character.url}
+              imageUrl={character.image_url}
+              voiceActors={character.voice_actors}
+              />
+          ))}
+          </Flex>
+
         </div>
       );
     }
@@ -117,15 +151,31 @@ function AnimeResult(props) {
         <h1>{props.title}</h1>
         <img src={props.imageUrl} alt=""></img>
         <p>Score: {props.score}</p>
-        <p>ScoredBy: {props.scoredBy}</p>
+        <p>Scored By: {props.scoredBy}</p>
         <p>Rank: {props.rank}</p>
-        <p>Popularity: {props.popularity}</p>        
+        <p>Popularity: {props.popularity}</p>
       </Flex>
     )
   } else {
     return null;
   }
+}
 
+function MainCharacter(props) {
+  console.log('props.voiceActors[0]: ' + props.voiceActors[0]);
+  console.log('props.voiceActors[0].image_url: ' + props.voiceActors[0].image_url);
+  return (
+    <Flex container margin="10px auto" justifyContent="space-around" flexWrap="wrap">
+      <Flex container flexDirection="column" alignItems="flex-start" alignItems="center"  padding="50px">
+            <h1>{props.name}</h1>
+            <a href={props.url} target="_blank" rel="noopener noreferrer"><img src={props.imageUrl} alt=""></img></a>
+      </Flex>
+      <Flex container flexDirection="column" alignItems="flex-start" alignItems="center"  padding="50px">
+            <h1>{props.voiceActors[0].name}</h1>
+            <a href={props.voiceActors[0].url} target="_blank" rel="noopener noreferrer"><img src={props.voiceActors[0].image_url} alt=""></img></a>
+      </Flex>
+      </Flex>
+  )
 }
 
 export default Search;
